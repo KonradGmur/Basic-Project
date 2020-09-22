226d5e63b01544b7556a85c804751225cda83db7
Konrad <konrad.gmur@gmail.com>
2020-09-22 19:37:12 +0200

    react test 4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React, { Component } from "react";
 import "./App.css";
 
-class Names extends Component {
-  render() {
-    const names = ["Konrad", "Basia", "Adam"];
-    const namesList = names.map((name) => <li>{name}</li>);
-    const showNames = true;
-    return <div>{showNames ? <ul>{namesList}</ul> : "Brak imion"}</div>;
-  }
-}
+const Names = () => {
+  const names = ["Konrad", "Basia", "Adam"];
+  const namesList = names.map((name) => <li>{name}</li>);
+  const showNames = true;
+  return <div>{showNames ? <ul>{namesList}</ul> : "Brak imion"}</div>;
+};
 
 class Headline extends Component {
+  state = {
+    title: "ReactJS",
+  };
+
   render() {
-    const title = "ReactJS";
+    const changeTitle = () => {
+      this.setState({
+        title: "Vue",
+      });
+    };
 
     return (
       <div>
-        <h1>Hello {title}</h1>
+        <h1>Hello {this.state.title}</h1>
         <Names />
+        <button onClick={changeTitle}>Change title</button>
       </div>
     );
   }
