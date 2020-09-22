0a1463580be9243da7eb3a9491106a27dd4c2f14
Konrad <konrad.gmur@gmail.com>
2020-09-22 19:49:23 +0200

    react test 5

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React, { Component } from "react";
 import "./App.css";
 
 const Names = () => {
+  const [showNameState, setShowNameState] = React.useState(true);
   const names = ["Konrad", "Basia", "Adam"];
-  const namesList = names.map((name) => <li>{name}</li>);
-  const showNames = true;
-  return <div>{showNames ? <ul>{namesList}</ul> : "Brak imion"}</div>;
+  const namesList = names.map((name) => <li key={name}>{name}</li>);
+
+  const toggleNames = () => {
+    setShowNameState(!showNameState);
+  };
+
+  return (
+    <div>
+      {showNameState ? <ul>{namesList}</ul> : "Brak imion"}
+      <button onClick={toggleNames}>Toggle names</button>
+    </div>
+  );
 };
 
 class Headline extends Component {
