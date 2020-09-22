e1f9aa5734348d885f283dffabb92da209c3c0ac
Konrad <konrad.gmur@gmail.com>
2020-09-22 19:31:44 +0200

    react test 3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from "react";
 import "./App.css";
 
-class Headline extends Component {
+class Names extends Component {
   render() {
-    const title = "ReactJS";
     const names = ["Konrad", "Basia", "Adam"];
     const namesList = names.map((name) => <li>{name}</li>);
     const showNames = true;
+    return <div>{showNames ? <ul>{namesList}</ul> : "Brak imion"}</div>;
+  }
+}
+
+class Headline extends Component {
+  render() {
+    const title = "ReactJS";
+
     return (
       <div>
         <h1>Hello {title}</h1>
-        {showNames ? <ul>namesList</ul> : null}
+        <Names />
       </div>
     );
   }
