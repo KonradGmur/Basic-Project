df00121893c78ba7dd118c67b5dcfee0ca840ae1
Konrad <konrad.gmur@gmail.com>
2020-09-22 19:20:39 +0200

    react test 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return <div>Hello</div>;
+class Headline extends Component {
+  render() {
+    const title = "ReactJS";
+    const names = ["Konrad", "Basia", "Adam"];
+
+    return (
+      <div>
+        <h1>Hello {title}</h1>
+        <ul>
+          <li>{names[0]}</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Headline;
